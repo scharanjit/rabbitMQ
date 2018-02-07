92519460d5069415bf896bfb0a4a3417e581c7fd
sich6002 <charanjit.singh@imaginea.com>
2018-02-07 11:37:47 +0530

    few changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rabbitMQ
+
+This is a demo project showing transfer of packets from Sender to Receiver using Rabbit MQ
+
+1.) Setup rabbitMQ-server @ ur Linux machine
+2.) Run the server
+ sudo rabbitmq-server start
+ 
+3.) Run both Recv and Send Command 
--- src/main/java/org/turvo/client/demo/Send.java
+++ src/main/java/org/turvo/client/demo/Send.java
@@ -13,19 +13,25 @@ public class Send {
         //set up connection to the server
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        int i = 0;
+        while (i <= 1000) {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
 
-        //declare the queue
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        String message = "Hello World!";
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-        System.out.println(" [x] Sent '" + message + "'");
+
+            //declare the queue
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World! " + i;
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+
 //        Declaring a queue is idempotent - it will only be created if it
 //         doesn't exist already. The message content is a byte array,
 //        so you can encode whatever you like there.
 
-        channel.close();
-        connection.close();
+            channel.close();
+            connection.close();
+            i++;
+        }
     }
 }
